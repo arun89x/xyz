d0b03be5ce0293cdbe36a41a79ee46121e316ac5
arun89x <104215331+arun89x@users.noreply.github.com>
2023-03-22 16:36:17 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,19 @@
+package abc;
+
+import java.util.Scanner;
+
+public class Sum {
+
+	public static void main(String[] args) {
+		
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter the value of X:");
+		int x=s.nextInt();
+		System.out.println("Enter the value of y:");
+		int y=s.nextInt();
+		
+		System.out.println("Sum is :"+(x+y));
+
+	}
+
+}
